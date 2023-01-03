e3fbb151dba82f8c57a648603b48b0fb1796de62
Luis Prates <lprates@student.42lisboa.com>
2023-01-03 20:21:31 +0000

    More on map container. Observers and modifiers

--- includes/map.hpp
+++ includes/map.hpp
@@ -6,7 +6,7 @@
 /*   By: lprates <lprates@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/25 01:32:28 by lprates           #+#    #+#             */
-/*   Updated: 2023/01/03 01:43:12 by lprates          ###   ########.fr       */
+/*   Updated: 2023/01/03 20:19:50 by lprates          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -174,6 +174,53 @@ namespace ft {
 					this->insert(*first++);
 			}
 
+			void	erase(iterator position) {
+				this->erase(position++, position);
+			}
+
+			size_type	erase(const key_type &k) {
+				iterator	element = this->find(k);
+
+				if (element == this->end())
+					return (0);
+				this->_btree_rm(element._node);
+				return (1);
+			}
+
+			void	erase(iterator first, iterator last) {
+				while (first != last)
+					this->_btree_rm((first++)._node);
+			}
+
+			void	swap(map &x) {
+				map	tmp;
+				
+				tmp._cpy_content(x);
+				x._cpy_content(*this);
+				this->_cpy_content(tmp);
+			}
+
+			void	clear(void) {
+				node_ptr	ghost = this->end()._node;
+
+				if (this->_size == 0)
+					return ;
+				ghost->parent->right = NULL;
+				this->_btree_clear(this->_data);
+				this->_data = ghost;
+				this->_size = 0;
+			}
+
+			// Observers
+
+			key_compare	key_comp(void) const {
+				return (key_compare());
+			}
+
+			value_compare	value_comp(void) const {
+				return (value_compare(key_compare()));
+			}
+
 			private:
 				node_ptr				_data;
 				key_compare				_key_cmp;
@@ -184,6 +231,83 @@ namespace ft {
 				template<class Ite>
 				void	_create_data_it(Ite first, Ite last) {
 					this->insert(first, last);
+				}
+
+				void	_btree_rm(node_ptr	node) {
+					node_ptr	replaceNode = NULL;
+					node_ptr	*rmPlace = &this->_data;
+
+					--this->_size;
+					if (node->parent)
+						rmPlace = (node->parent->left == node ? &node->parent->left : & node->parent->right);
+					if (node->left == NULL && node->right == NULL)
+						;
+					else if (node->left == NULL)
+						replaceNode = node->right;
+					else {
+						replaceNode = farRight(node->left);
+						if (replaceNode != node->left)
+							if (replaceNode->parent->right = replaceNode->left)
+								replaceNode->left->parent = replaceNode->parent;
+					}
+					if (replaceNode) {
+						replaceNode->parent = node->parent;
+						if (node->left && node->left != replaceNode) {
+							replaceNode->left = node->left;
+							replaceNode->left->parent = replaceNode;
+						}
+						if (node->right && node->right != replaceNode) {
+							replaceNode->right = node->right;
+							replaceNode->right->parent = replaceNode;
+						}
+					}
+					*rmPlace = replaceNode;
+					delete node;
+				}
+
+				void	_btree_add(node_ptr newNode) {
+					node_ptr	*parent = &this->_data;
+					node_ptr	*node = &this->_data;
+					node_ptr	ghost = farRight(this->_data);
+					bool		sideLeft = -1;
+					
+					++this->size;
+					while (*node && *node != ghost) {
+						parent = node;
+						sideLeft = this->_key_cmp(newNode->data.first, (*node)->data.first);
+						node = (sideLeft ? &(*node)->left : &(*node)->right);
+					}
+					if (*node == NULL) {
+						newNode->parent = (*parent);
+						*node = newNode;
+					}
+					else {
+						*node = newNode;
+						newNode->parent = ghost->parent;
+						ghost->parent = farRight(newNode);
+						farRight(newNode)->right = ghost;
+					}
+				}
+
+				void	_btree_clear(node_ptr node) {
+					if (node == NULL)
+						return ;
+					this->_btree_clear(node->left);
+					this->_btree_clear(node->right);
+					delete node;
+				}
+
+				void	_cpy_content(map &src) {
+					this->clear();
+					node_ptr tmp = this->_data;
+					
+					this->_data = src._data;
+					this->_key_cmp = src._key_cmp;
+					this->_alloc = src._alloc;
+					this->_size = src._size;
+					src._data = tmp;
+					src._size = 0;
+					tmp = NULL;
 				}
 
 			
